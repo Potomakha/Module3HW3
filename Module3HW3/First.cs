a99b60bf609db4a26f6b990f42d5e4de89f5beb4
Oleh Potomakha <potomakha@gmail.com>
2021-12-12 17:30:22 +0200

    First and Second classes

---
+++ Module3HW3/First.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW3
+{
+    public class First
+    {
+        private Action<bool> _actionHandler;
+
+        public First()
+        {
+        }
+
+        public First(Action<bool> action)
+        {
+            Register(action);
+        }
+
+        public void Register(Action<bool> action)
+        {
+            _actionHandler += action;
+        }
+
+        public void UnRegister(Action<bool> action)
+        {
+            _actionHandler -= action;
+        }
+
+        public void ActionInvoke(bool state)
+        {
+            _actionHandler(state);
+        }
+
+        public double Pow(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Module3HW3/Second.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW3
+{
+    public class Second
+    {
+        private double _operationResult;
+        public Predicate<double> Clalc(Func<double, double, double> operation, double x, double y)
+        {
+            _operationResult = operation(x, y);
+            return new Predicate<double>(Result);
+        }
+
+        private bool Result(double value)
+        {
+            var residue = _operationResult % value;
+
+            if (residue.Equals(0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
