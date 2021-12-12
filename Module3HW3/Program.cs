41888613b04e5753951c5b7e09ae80d9480246bf
Oleh Potomakha <potomakha@gmail.com>
2021-12-12 17:31:57 +0200

    use our classes

--- Module3HW3/Program.cs
+++ Module3HW3/Program.cs
@@ -6,7 +6,21 @@ namespace Module3HW3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var first = new First(Show);
+            var second = new Second();
+
+            Predicate<double> resultHandler = second.Clalc(first.Pow, 5, 30);
+
+            var state = resultHandler.Invoke(33);
+            first.ActionInvoke(state);
+
+            state = resultHandler.Invoke(1);
+            first.ActionInvoke(state);
+        }
+
+        public static void Show(bool state)
+        {
+            Console.WriteLine($"is shares entirely? - {state}");
         }
     }
 }
